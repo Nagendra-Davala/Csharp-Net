8799fdf78704e86b5fe85a44235051ae02ca529c
Nagendra <87424058+Nagendra-Davala@users.noreply.github.com>
2025-09-23 15:12:00 +0530

    Day1 & Day2

---
+++ Day1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.Net
+{
+    public class Day1
+    {
+        public void SumOfTwoNumbers()
+        {
+            var number1 = Convert.ToInt32(Console.ReadLine());
+            var number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Sum : {number1}+ {number2} = {number1 + number2}");
+        }
+        public void IsCharExistInString() 
+        {
+            var inputString = Console.ReadLine();
+            var inputChar = Console.ReadKey().KeyChar;
+            if (inputString != null && inputString.Contains(Convert.ToChar(inputChar)))
+            {
+                Console.WriteLine($"{inputChar} exists in {inputString}");
+            }
+        }
+        public void PrintTriangle()
+        {
+            Console.WriteLine("Enter a number");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+   
+}
---
+++ Day2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.Net
+{
+    public class Day2
+    {
+        public void IsPositiveOrNegativeOrZero()
+        {
+            Console.WriteLine("Enter a number to check whether it is positive, Negative or zero.");
+            var number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0) 
+            {
+                Console.WriteLine($"{number} is a Positive Integer.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"{number} is a Negative Integer.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is a Zero.");
+            }
+           
+        }
+
+        public void IsVowelOrConsonant()
+        {
+            Console.WriteLine("Enter a character to check it is vowel or consonant.");
+            var character = Convert.ToChar(Console.ReadKey().KeyChar);
+            if ("aeiouAEIOU".Contains(character)) 
+            {
+                Console.WriteLine($"{character} is vowel");
+            }
+            else
+            {
+                Console.WriteLine($"{character} is Consonants");
+            }
+        }
+
+        public void CountOfDigitsInNumber() 
+        {
+            Console.WriteLine("Enter a number to count number of digits.");
+            var originalNumber = Convert.ToInt32(Console.ReadLine());
+            int number = Math.Abs(originalNumber);
+            int count = 0;
+            while (number > 0) 
+            {
+                number /= 10;
+                count++;
+            }
+            Console.WriteLine($"Count of digits in {originalNumber} : {count}");
+        }
+
+        public void ReverseNumber()
+        {
+            Console.WriteLine("Enter a number to reverse number of digits.");
+            var originalNumber = Convert.ToInt32(Console.ReadLine());
+            int number = Math.Abs(originalNumber);
+            int reverseNumber = 0;
+            while (number > 0)
+            {
+                reverseNumber = (number % 10) + (reverseNumber * 10);
+                number /= 10;
+            }
+
+            if(originalNumber < 0)
+            {
+                reverseNumber *= -1;
+            }
+            Console.WriteLine($"reverse of digits in {originalNumber} : {reverseNumber}");
+        }
+
+        public void SumOfDigitsInNumber()
+        {
+            Console.WriteLine("Enter a number to sum number of digits.");
+            var originalNumber = Convert.ToInt32(Console.ReadLine());
+            int number = Math.Abs(originalNumber);
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            Console.WriteLine($"Sum of digits in {originalNumber} : {sum}");
+        }
+    }
+}
